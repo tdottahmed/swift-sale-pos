2e165fc15b56a53472c878f7a19cde5aad31a69b
Tanbir Ahmed <pondit.tanbir@gmail.com>
2024-02-13 00:05:55 +0600

    guest blade conflict ressolve

---
+++ resources/views/components/layouts/guest.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.head/>
+<body>				
+	<!-- Page content -->
+	<div class="page-content">
+            <div class="content pt-0">
+                {{$slot}}
+            </div>
+	</div>
+    <!-- Page content -->
+</body>
+</html>
