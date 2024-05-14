a9c990a5789b4a76ba18a72da5876785045aab03
MD. NAJMUL SARKER <najmulsarker.cse@gmail.com>
2024-05-15 00:24:28 +0600

    single product

--- app/Http/Controllers/Frontend/Porto/FrontendController.php
+++ app/Http/Controllers/Frontend/Porto/FrontendController.php
@@ -4,22 +4,28 @@ namespace App\Http\Controllers\Frontend\Porto;
 
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('frontend.porto.index');
     }
 
-    public function singleProduct(){
-        return view('frontend.porto.product.single-product');
+    public function singleProduct()
+    {
+
+        // $product=Product::first('uuid',$product->uuid);
+        // $relatedProduct=Product::where('category',$product->category)->get();
+
+        $product = Product::first();
+        return view('frontend.porto.product.single-product', compact('product'));
     }
 
-    public function products(){
+    public function products()
+    {
         return view('frontend.porto.product.products');
     }
-   
-    
-
 }
