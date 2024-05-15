c77c01250d811b1a382866e756b5602d3b584df2
rifatmdashrafulislam <rifatmdashrafulislam@gmail.com>
2024-05-15 22:51:04 +0600

    Cart & checkout

--- app/Http/Controllers/Frontend/Porto/DashboardController.php
+++ app/Http/Controllers/Frontend/Porto/DashboardController.php
@@ -11,6 +11,15 @@ class DashboardController extends Controller
     public function index(){
         return view('frontend.porto.index');
     }
+
+
+    public function card(){
+        return view('frontend.porto.card');
+    }
+
+    public function checkout(){
+        return view('frontend.porto.checkout');
+    }
    
     
 
--- routes/frontend.php
+++ routes/frontend.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\Frontend\Porto\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('frontend.index');
+//card
+Route::get('/card', [DashboardController::class, 'card'])->name('frontend.card');
+//checkout
+Route::get('/checkout', [DashboardController::class, 'checkout'])->name('frontend.checkout');
