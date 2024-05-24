704784da64d2eefa943ce456423fc1f874571486
MD. NAJMUL SARKER <najmulsarker.cse@gmail.com>
2024-05-25 00:22:13 +0600

    hrm all table model andj migration

---
+++ database/migrations/2024_05_24_224548_create_employees_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->nullable();
+            $table->foreignId('branch_id')->constrained()->onUpdate('cascade')->onDelete('cascade')->nullable();
+            $table->foreignId('department_id')->constrained()->onUpdate('cascade')->onDelete('cascade')->nullable();
+            $table->foreignId('role_id')->constrained()->onUpdate('cascade')->onDelete('cascade')->nullable();
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->string('email')->nullable();
+            $table->date('dob')->nullable();
+            $table->integer('nid')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('staff_id')->nullable();
+            $table->string('user_name')->nullable();
+            $table->string('password')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
