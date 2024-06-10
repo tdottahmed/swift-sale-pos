d0c943bad177a2d5b71892ccee7479b2ac0d5c09
Tanbir Ahmed <pondit.tanbir@gmail.com>
2024-06-10 23:29:43 +0600

    whistlist migration fixed

--- database/migrations/2024_06_07_202348_create_wishlists_table.php
+++ database/migrations/2024_06_07_202348_create_wishlists_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('cascade');
-            $table->foreignId('product_id')->constrained('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
+        
     }
 
     /**
